2a9e337de1b0fd29518628f2e38a8963d6aa61d3
veronica Pillajo <vpillajon@est.ups.edu.ec>
2019-12-18 12:39:18 -0500

    Commit inicial

---
+++ app2.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+
+const ubicacion = require('./controlador/ubicacion');
+const clima = require('./controlador/clima');
+
+const argv = require('yargs').options({
+    nombre: {
+        alias: 'n',
+        desc: 'Nombre de la ciudad para obtener el clima',
+        demand: true
+    }
+}).argv;
+
+const getInfo = async(ciudad) => {
+    try {
+        const coords = await ubicacion.getCiudadLatLon(ciudad);
+        const temp = await clima.getClima(coords.lat, coords.lon);
+        return `El clima de ${ coords.direccion } es de ${ temp }.`;
+    } catch (e) {
+        return `No se pudo determinar el clima de ${ ciudad }`;
+    }
+}
+
+getInfo(argv.nombre)
+    .then(console.log)
+    .catch(console.log);
